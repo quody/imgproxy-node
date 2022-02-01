b4b912d85b298c68e8b0064caf4fea96d21aa32d
quody <aleksi.o.jokela@gmail.com>
2022-02-01 15:34:58 +0200

    feat: use cryptojs

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,19 @@
-import crypto from 'crypto';
+import * as CryptoJS from "crypto-js";
+import { Buffer } from "buffer";
 import {
   FocusPoint,
   ImgproxySecureConfig,
   OffsetGravity,
   RGBColor,
 } from './types';
+
+const createHmac = (encoding, privateKey) => {
+  const key = CryptoJS.enc.Utf8.parse(privateKey);
+  const ts = new Date().getTime();
+  const timestamp = CryptoJS.enc.Utf8.parse(ts);
+  const hmac = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA256(timestamp, key));
+  return hmac;
+};
 
 export const isRGBColor = (obj: any): obj is RGBColor => {
   return typeof obj === 'object' && 'r' in obj && 'g' in obj && 'b' in obj;
@@ -42,7 +51,7 @@ export const sign = (
   target: string,
   size: number = 32
 ) => {
-  const hmac = crypto.createHmac('sha256', hexDecode(key));
+  const hmac = createHmac('sha256', hexDecode(key));
   hmac.update(hexDecode(salt));
   hmac.update(target);
   return urlSafeEncode(hmac.digest().slice(0, size));
